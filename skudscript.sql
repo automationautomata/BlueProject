91a2d86bda4701046646ae94d978d494e7faf3cd
Алексей <@DESKTOP-IJJ0HOH>
2024-07-19 22:08:18 +0400

    method for arduino connection created

---
+++ skudscript.sql
@@ -0,0 +1,28 @@
+CREATE TABLE cards (
+    id VARCHAR(4) PRIMARY KEY,
+    isSabotaged VARCHAR(1) NOT NULL
+);
+CREATE TABLE rooms (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL
+);
+CREATE TABLE rights (
+    id INTEGER NOT NULL AUTOINCREMENT,
+    name TEXT NOT NULL
+);
+CREATE TABLE access_rules (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    right INTEGER NOT NULL,
+    room INTEGER NOT NULL,
+    FOREIGN KEY(room) REFERENCES rooms(id),
+    FOREIGN KEY(right) REFERENCES rights(id)
+    UNIQUE(right, room)
+);
+CREATE TABLE entities (
+    card VARCHAR(4),
+    sid INTEGER PRIMARY KEY,
+    type VARCHAR(1) NOT NULL,
+    right INTEGER NOT NULL,
+    FOREIGN KEY(card) REFERENCES cards(id),
+    FOREIGN KEY(right) REFERENCES rights(id)
+);
