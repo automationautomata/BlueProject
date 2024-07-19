e5b5cd6edae8aa8e08e6188dd21de00662c7ac97
Алексей <@DESKTOP-IJJ0HOH>
2024-07-20 03:29:47 +0400

    added date_time column to all tables in skudscriptdb

--- SKUD/skudscript.sql
+++ SKUD/skudscript.sql
@@ -1,33 +1,33 @@
 CREATE TABLE cards (
     id VARCHAR(4) PRIMARY KEY,
-    isSabotaged VARCHAR(1) NOT NULL
+    isSabotaged VARCHAR(1) NOT NULL,
     date_time TEXT NOT NULL
 );
 CREATE TABLE rooms (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL
+    name TEXT NOT NULL,
     date_time TEXT NOT NULL
 );
 CREATE TABLE rights (
     id INTEGER NOT NULL AUTOINCREMENT,
-    name TEXT NOT NULL
+    name TEXT NOT NULL,
     date_time TEXT NOT NULL
 );
 CREATE TABLE access_rules (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     right INTEGER NOT NULL,
     room INTEGER NOT NULL,
+    date_time TEXT NOT NULL,
     FOREIGN KEY(room) REFERENCES rooms(id),
-    FOREIGN KEY(right) REFERENCES rights(id)
+    FOREIGN KEY(right) REFERENCES rights(id),
     UNIQUE(right, room)
-    date_time TEXT NOT NULL
 );
 CREATE TABLE entities (
     card VARCHAR(4),
     sid INTEGER PRIMARY KEY,
     type VARCHAR(1) NOT NULL,
     right INTEGER NOT NULL,
+    date_time TEXT NOT NULL,
     FOREIGN KEY(card) REFERENCES cards(id),
     FOREIGN KEY(right) REFERENCES rights(id)
-    date_time TEXT NOT NULL
 );
