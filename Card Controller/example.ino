e0810c0d02af3b5ae2dc637061da3ffe508dd775
Алексей <@DESKTOP-IJJ0HOH>
2024-07-30 03:41:14 +0400

    test version

--- Card Controller/example.ino
+++ Card Controller/example.ino
@@ -1,24 +1,41 @@
 #include <ArduinoJson.h>
 
-JsonDocument doc;
+String parsjson(String jsondata) {
+    DeserializationError err = deserializeJson(recieve_doc, jsondata);
+    return err.c_str();
+}
 
 void setup() {
-  // Начинаем работу с Serial портом на скорости 9600 бит/с
   Serial.begin(9600);
-  // Пример заполнения
-  doc["sensor"] = "gps";
-  doc["time"]   = 1351824120;
-  doc["data"][0] = 48.756080;
-  doc["data"][1] = 2.302038;
-
 }
 // При работе НЕ с ардуино леонардо можно использовать SerialEvent, 
 // а в loop просто проверять данные с датчика
 void loop() {
   // Проверяем, есть ли данные из Serial порта
   if (Serial.available() > 0) {
+    JsonDocument doc;
     String receivedData = Serial.readString();
     serializeJson(doc, receivedData);
-    Serial.println("Получено: dd" + receivedData);
   }
+  
+  if (random(30001) >= 30000) {
+    String sendingdata = Serial.readString();
+    serializeJson(getkeys(), sendingdata);
+    Serial.print(sendingdata)
+  }
+}
+
+void savekeys(JsonDocument doc) {
+  JsonArray array = doc["keys"].as<JsonArray>();
+  for (int i = 0; i < array.size(); i++) {
+    EEPROM.write(i*sizeof(size_t), array[i]);
+  }
+}
+
+JsonDocument getkeys() {
+  JsonDocument array;
+  for (int i = 0; i < array.size(); i++) {
+    EEPROM.read(i*sizeof(size_t), array["keys"][i]);
+  }
+  return array
 }
